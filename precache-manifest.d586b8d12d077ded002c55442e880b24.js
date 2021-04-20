613910d585dc86b5230924031775c9491de4e6ac
Gaurav Thapa <young.gaurav123@gmail.com>
2021-04-20 18:51:42 +0545

    Updates

---
+++ precache-manifest.d586b8d12d077ded002c55442e880b24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9dad100e655005a6e2d67afbbe6d32b",
+    "url": "/covidstat/index.html"
+  },
+  {
+    "revision": "d92d7ed0b5cc89afc6b8",
+    "url": "/covidstat/static/css/main.8b359ef5.chunk.css"
+  },
+  {
+    "revision": "729b89e5fdb7db87da30",
+    "url": "/covidstat/static/js/2.db22fc36.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covidstat/static/js/2.db22fc36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d92d7ed0b5cc89afc6b8",
+    "url": "/covidstat/static/js/main.b85b9c94.chunk.js"
+  },
+  {
+    "revision": "18946fd6b4db890c2ab5",
+    "url": "/covidstat/static/js/runtime-main.d63d0d8b.js"
+  },
+  {
+    "revision": "6ce9074d039177a5b09ef64551377167",
+    "url": "/covidstat/static/media/covid-19.6ce9074d.png"
+  }
+]);
